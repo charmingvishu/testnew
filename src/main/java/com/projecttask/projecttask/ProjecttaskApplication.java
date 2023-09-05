e5403a8725fb6f853efa0bb8a0c3511912f84b9c
Vishakha <parkhevishakha315@gmail.com>
2023-09-05 15:23:16 +0530

    upload

---
+++ src/main/java/com/projecttask/projecttask/ProjecttaskApplication.java
@@ -0,0 +1,19 @@
+package com.projecttask.projecttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecttaskApplication.class, args);
+		System.out.println("Server Status On.......");
+
+System.out.println("Vishu**************************************");
+
+
+
+	}
+
+}
